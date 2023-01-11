c25638381b8d6360c438a4bef0e57d3b4245cab2
sozerodev <sozerodev@gmail.com>
2023-01-11 11:12:27 +0900

    primitive types

---
+++ lecture2/index.js
@@ -0,0 +1,29 @@
+var thisName = "kim";
+var age = 50;
+var married = true;
+// 크게 쓸데는 없고 걍 가능은 함
+var isNull = null;
+var isDefined = undefined;
+// array자료에 타입 지정 가능
+var members = ["kim", "park"];
+var members2 = ["kim", 3, true];
+// 변수 하나에 여러 자료 집어넣고 싶으면 object자료형을 써도 가능
+var members3 = {
+    member1: "kim",
+    member2: "park",
+};
+// Question1
+var whatMyName = "so";
+var whatMyAge = 30;
+var whereBorn = "U";
+// Question2
+var musicInfo = {
+    title: "anything",
+    singer: "anywhom",
+};
+// Question3
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
---
+++ lecture2/index.ts
@@ -0,0 +1,47 @@
+let thisName: string = "kim";
+
+let age: number = 50;
+
+let married: boolean = true;
+
+// 크게 쓸데는 없고 걍 가능은 함
+let isNull: null = null;
+let isDefined: undefined = undefined;
+
+// array자료에 타입 지정 가능
+let members: string[] = ["kim", "park"];
+let members2: [string, number, boolean] = ["kim", 3, true];
+
+// 변수 하나에 여러 자료 집어넣고 싶으면 object자료형을 써도 가능
+let members3: {
+  member1: string;
+  member2: string;
+} = {
+  member1: "kim",
+  member2: "park",
+};
+
+// Question1
+let whatMyName: string = "so";
+let whatMyAge: number = 30;
+let whereBorn: string = "U";
+
+// Question2
+const musicInfo: {
+  title: string;
+  singer: string;
+} = {
+  title: "anything",
+  singer: "anywhom",
+};
+
+// Question3
+let project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
